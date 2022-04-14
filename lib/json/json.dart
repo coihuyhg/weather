dbf2de3340e302c54f0f33b43e1d4f5fb32e51e6
Huy <kenhgvn321@gmail.com>
2022-04-14 11:49:35 +0700

    camket

---
+++ lib/json/json.dart
@@ -0,0 +1,131 @@
+const List<Map<String, Object>> place = [
+  {
+    'location' : 'Malang',
+    'ic' : 'assets/images/ic_map-pin.png',
+    'temperature' : '20°/24°',
+    'img' : 'assets/images/ic_black_weather-rain-showers.png',
+    'weather' : 'Heavy rain',
+  },
+  {
+    'location' : 'Banyuwangi',
+    'ic' : 'assets/images/ic_checkbox=null.png',
+    'temperature' : '20°/24°',
+    'img' : 'assets/images/ic_black_weather-rain-showers.png',
+    'weather' : 'Heavy rain',
+  },
+  {
+    'location' : 'Jakarta',
+    'ic' : 'assets/images/ic_checkbox=null.png',
+    'temperature' : '27°/28°',
+    'img' : 'assets/images/ic_black_weather-partly-cloudy.png',
+    'weather' : 'Cloudy',
+  },
+];
+
+const List<Map<String, Object>> infoWeather = [
+  {
+    'time': 'Now',
+    'img': 'assets/images/ic_white_weather-rain-showers.png',
+    'temperature': '20°/24°',
+    'percent': '74% rain',
+  },
+  {
+    'time': '10:00',
+    'img': 'assets/images/ic_white_weather-rain-showers.png',
+    'temperature': '20°/24°',
+    'percent': '69% rain',
+  },
+  {
+    'time': '11:00',
+    'img': 'assets/images/ic_white_weather-rain-showers.png',
+    'temperature': '20°/24°',
+    'percent': '62% rain',
+  },
+  {
+    'time': '12:00',
+    'img': 'assets/images/ic_weather-snow-shower.png',
+    'temperature': '20°/24°',
+    'percent': '53% rain',
+  },
+  {
+    'time': '13:00',
+    'img': 'assets/images/ic_weather-snow-shower.png',
+    'temperature': '22°/24°',
+    'percent': '45% rain',
+  },
+  {
+    'time': '14:00',
+    'img': 'assets/images/ic_weather-snow-shower.png',
+    'temperature': '22°/24°',
+    'percent': '43% rain',
+  },
+  {
+    'time': '15:00',
+    'img': 'assets/images/ic_white_weather-partly-cloudy.png',
+    'temperature': '23°/24°',
+    'percent': '33% rain',
+  },
+  {
+    'time': '16:00',
+    'img': 'assets/images/ic_white_weather-partly-cloudy.png',
+    'temperature': '23°/24°',
+    'percent': '23% rain',
+  },
+  {
+    'time': '17:00',
+    'img': 'assets/images/ic_white_weather-partly-cloudy.png',
+    'temperature': '23°/24°',
+    'percent': '22% rain',
+  },
+  {
+    'time': '18:00',
+    'img': 'assets/images/ic_weather-partly-cloudy-night.png',
+    'temperature': '23°/24°',
+    'percent': '20% rain',
+  },
+];
+
+const List<Map<String, Object>> forecast = [
+  {
+  'title' : 'Sun',
+  'img' : 'assets/images/ic_white_weather-rain-showers.png',
+  'percent' : '74% rain',
+  'temperature' : '23°/24°',
+  },
+  {
+    'title' : 'Mon',
+    'img' : 'assets/images/ic_weather-sunny.png',
+    'percent' : '13% rain',
+    'temperature' : '23°/27°',
+  },
+  {
+    'title' : 'Tues',
+    'img' : 'assets/images/ic_white_weather-partly-cloudy.png',
+    'percent' : '32% rain',
+    'temperature' : '22°/25°',
+  },
+  {
+    'title' : 'Wed',
+    'img' : 'assets/images/ic_white_weather-rain-showers.png',
+    'percent' : '55% rain',
+    'temperature' : '20°/24°',
+  },
+  {
+    'title' : 'Thurs',
+    'img' : 'assets/images/ic_white_weather-rain-showers.png',
+    'percent' : '53% rain',
+    'temperature' : '20°/24°',
+  },
+  {
+    'title' : 'Fri',
+    'img' : 'assets/images/ic_white_weather-partly-cloudy.png',
+    'percent' : '32% rain',
+    'temperature' : '20°/24°',
+  },
+  {
+    'title' : 'Sat',
+    'img' : 'assets/images/ic_weather-sunny.png',
+    'percent' : '18% rain',
+    'temperature' : '23°/27°',
+  },
+];
